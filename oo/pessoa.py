058628bc4ab9f27540b1e811d8d23cfb962b7813
ElizelBrito <elisbel13@gmail.com>
2025-07-22 13:56:02 -0300

    Criados atributos de instâcia nome e idada

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 46):
+        self.idade = idade
+        self.nome = None
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__' :
-    p =Pessoa()
+    p = Pessoa('Elizel')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Elizel'
+    print(p.nome)
+    print(p.idade)
