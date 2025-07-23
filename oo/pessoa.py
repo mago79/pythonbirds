bb8a418a9179343c9642545f0991b9cc49c49232
ElizelBrito <elisbel13@gmail.com>
2025-07-22 21:32:50 -0300

    Criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 46):
+    def __init__(self, *filhos, nome = None, idade = 46):
         self.idade = idade
-        self.nome = None
+        self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__' :
-    p = Pessoa('Elizel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Elizel'
-    print(p.nome)
-    print(p.idade)
+    elizel = Pessoa(nome = 'Elizel')
+    luciano = Pessoa(elizel, nome = 'luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
